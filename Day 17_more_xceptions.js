dc86ba1d7a6b429a4ef4f909eeedbf6c88b3e054
jose marte <tumusicalatina@gmail.com>
2019-05-26 19:28:41 -0400

    Day 17

---
+++ Day 17_more_xceptions.js
@@ -0,0 +1,13 @@
+function Calculator() {
+  this.power = function(n, p) {
+    try {
+      if (n >= 0 && p >= 0) {
+        return Math.pow(n, p);
+      } else {
+        throw "n and p should be non-negative";
+      }
+    } catch (err) {
+      return err;
+    }
+  };
+}
